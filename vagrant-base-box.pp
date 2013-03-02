233ac577e06c831ca1cc47c4a567fc361156e3e5
Manfred Stock <m@nfred.ch>
2013-03-02 23:42:46 +0100

    Add a simple manifest to setup a base box
    Manifest configures sudo and ssh and also installs Puppet, Chef and some other
    useful packages.

---
+++ vagrant-base-box.pp
@@ -0,0 +1,53 @@
+package {
+	['sudo', 'tmux', 'git', 'puppet', 'chef']:
+		ensure => present;
+	'nano':
+		ensure => absent;
+}
+
+group {
+	'admin':
+		ensure => present;
+}
+
+user {
+	'vagrant':
+		ensure     => present,
+		managehome => false,
+		groups     => ['cdrom', 'floppy', 'audio', 'dip', 'video', 'plugdev', 'admin'],
+		require    => Group['admin'];
+}
+
+file {
+	'/etc/sudoers.d/vagrant':
+		ensure  => present,
+		content => "%admin ALL=NOPASSWD: ALL\nDefaults env_keep += \"SSH_AUTH_SOCK\"\n",
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0600,
+		require => Package['sudo'];
+}
+
+ssh_authorized_key {
+	'vagrant':
+		ensure => present,
+		key    => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ==',
+		type   => 'ssh-rsa',
+		user   => 'vagrant',
+		name   => 'vagrant insecure public key';
+}
+
+augeas {
+	"no-dns-in-ssh":
+		context => "/files/etc/ssh/sshd_config",
+		changes => [
+			"set UseDNS no",
+		],
+}
+
+service {
+	'chef-client':
+		ensure  => stopped,
+		enable  => false,
+		require => Package['chef'];
+}
